cc8a316dbbe7c945461450b711819a553aab1732
pranish9 <pranishtuladhar9@gmail.com>
2023-04-27 15:38:00 +0545

    second assignment of mine in flutterbootcamp

---
+++ secoassign.dart
@@ -0,0 +1,11 @@
+void main() {
+//1. Declare a variable named myNumber and assign it a number value. Print the data type of the variable.
+  int myNumber = 89;
+  print(" the data type of myNumber : ${myNumber.runtimeType}");
+//2. Declare a variable named myString and assign it a string value. Print the data type of the variable.
+  String myString = "Nepal won ACC premiere cup 2023 ";
+  print(" the data type of myString : ${myString.runtimeType}");
+// 3. Declare a variable named myBoolean and assign it a boolean value. Print the data type of the variable.
+  bool myBoolean = false;
+  print(" the data type of myBoolean : ${myBoolean.runtimeType}");
+}
