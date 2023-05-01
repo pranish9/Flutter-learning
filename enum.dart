e45fc68e1709e1a26431e78e4787ebe1cf905b72
pranish9 <pranishtuladhar9@gmail.com>
2023-05-01 20:24:59 +0545

    information of enum in dart

---
+++ enum.dart
@@ -0,0 +1,17 @@
+enum Roles { user, admin, member, staff }
+
+enum Correction { looading, done, error }
+
+void main() {}
+checkingroles(Roles role) {
+  switch (role) {
+    case Roles.user:
+      break;
+    case Roles.admin:
+      break;
+    case Roles.member:
+      break;
+    case Roles.staff:
+      break;
+  }
+}
