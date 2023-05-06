1f162f9e7665f1ea0e2b4ef6846071bd2d409cb0
pranish9 <pranishtuladhar9@gmail.com>
2023-05-06 22:41:41 +0545

    assignment function completed

--- sixthassignment.dart
+++ sixthassignment.dart
@@ -1,7 +1,8 @@
 //1. Write a function that takes two numbers as a arguments and return the sum of the numbers passed.
 // Store the reutrn value in a variable and print the value that variable has.
-import 'dart:io';
+
 /*
+import 'dart:io';
 void main() {
   print("enter two number :\t");
   int num1 = int.parse(stdin.readLineSync()!);
@@ -20,7 +21,9 @@ int Sum(int a, int b) {
 //2. Write an arrow function to return the square of the number passed and print the value
 //returned by function storing in variable
 /*
+import 'dart:io';
 void main() {
+
   print("enter ay number :\t");
   int num = int.parse(stdin.readLineSync()!);
   int sqr = square(num);
@@ -31,6 +34,7 @@ int square(int num) => num * num; // arrow function
 */
 //3. Create an anonymous function that have to have 2 parameters which are integers. that sums the 2 number passed
 /*
+import 'dart:io';
 void main() {
   Function(int, int) sum = (int a, int b) => a + b;
   int result = sum(5, 10);
@@ -38,6 +42,8 @@ void main() {
 }
 */
 //4. Write a function that checks if the number passed is even, odd, negative or zero.
+/*
+import 'dart:io';
 void main() {
   print("enter any number :\t");
   int numb = int.parse(stdin.readLineSync()!);
@@ -55,6 +61,23 @@ int find(int a) {
     print("the number is odd");
   return 0;
 }
+*/
 
 //5. Write a higher order function that accepts the numbers and prints the sum of the number until the number becomes zero.
 //( eg. if a number is passed is 6 then 6+5+4+3+2+1 is what needs to be printed ). Hint :  use the recursive higher order function
+/*void main() {
+  void printSum(int num) {
+    void sum(int n, int acc) {
+      if (n == 0) {
+        print(acc);
+      } else {
+        sum(n - 1, acc + n);
+      }
+    }
+
+    sum(num, 0);
+  }
+
+  printSum(6);
+}
+*/
